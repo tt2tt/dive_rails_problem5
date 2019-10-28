6ba0d85d52416d9f326b4d609baca4a91ddaf7b3
”taro <groveleffect@gmail.com>
2019-10-28 22:05:14 +0900

    微調整

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module Manyou
 
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-    config.assets.initialize_on_precompile = false
 
     config.generators do |g|
       g.test_framework :rspec,
