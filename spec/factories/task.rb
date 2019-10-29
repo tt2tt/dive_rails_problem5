88c74d63734436f8621f70c5152ae1380581944b
”taro <groveleffect@gmail.com>
2019-10-29 22:32:38 +0900

    検索機能の追加

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -4,23 +4,27 @@ FactoryBot.define do
     name { 'test_task_01' }
     detail { 'test1' }
     deadline {'2014-10-28 07:24:00'}
+    status { '未着手' }
   end
 
   factory :second_task, class: Task do
     name { 'test_task_02' }
     detail { 'sample2' }
     deadline {'2015-10-28 07:24:00'}
+    status { '着手中' }
   end
 
   factory :third_task, class: Task do
     name { 'test_task_03' }
     detail { 'mof3' }
     deadline { '2016-10-28 07:24:00' }
+    status { '完了' }
   end
 
   factory :forth_task, class: Task  do
-    name { 'test_task_04' }
+    name { 'test_task_01' }
     detail { 'manyou4' }
     deadline {'2014-10-28 07:22:00'}
+    status { '完了' }
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,33 +1,27 @@
 require 'rails_helper'
 require 'date'
 
-background do
-  FactoryBot.create(:task)
-  FactoryBot.create(:second_task)
-  FactoryBot.create(:third_task)
-  FactoryBot.create(:forth_task)
-  time = Time.now
-end
-
 RSpec.describe Task, type: :model do
 
+  time = Time.now
+
   it 'nameが空ならバリテーションが通らない' do
-    task =  Task.new(name: '', detail: '仮タスク詳細', deadline: time)
+    task =  Task.new(name: '', detail: '仮タスク詳細', deadline: time, status: '未着手')
     expect(task).not_to be_valid
   end
 
   it 'detailがからならバリテーションが通らない' do
-    task =  Task.new(name: '仮タスク名', detail: '', deadline: time)
+    task =  Task.new(name: '仮タスク名', detail: '', deadline: time, status: '着手中')
     expect(task).not_to be_valid
   end
 
   it 'nameが51文字以上ならバリテーションが通らない' do
-    task =  Task.new(name: 'a' * 51, detail: '仮タスク詳細', deadline: time)
+    task =  Task.new(name: 'a' * 51, detail: '仮タスク詳細', deadline: time, status: '完了')
     expect(task).not_to be_valid
   end
 
   it 'detailが201文字以上ならバリテーションが通らない' do
-    task = Task.new(name: '仮タスク名' , detail: 'a' * 201, deadline: time)
+    task = Task.new(name: '仮タスク名' , detail: 'a' * 201, deadline: time, status: '完了')
     expect(task).not_to be_valid
   end
 
@@ -35,4 +29,38 @@ RSpec.describe Task, type: :model do
     task = Task.new(name: '仮タスク名', detail: '仮タスク詳細', deadline: time)
     expect(task).to be_valid
   end
+
+  it 'タスク名のみで絞り込みができる' do
+    FactoryBot.create(:task)
+    result1 = FactoryBot.create(:second_task)
+    FactoryBot.create(:third_task)
+    FactoryBot.create(:forth_task)
+
+    result = Task.search(task = {name: 'test_task_02', status: ''})[0]
+
+    expect(result).to eq result1
+  end
+
+  it 'ステータスのみで絞り込みができる' do
+    result1 = FactoryBot.create(:task)
+    FactoryBot.create(:second_task)
+    FactoryBot.create(:third_task)
+    FactoryBot.create(:forth_task)
+
+    result = Task.search(task = {name: '', status: '未着手'})[0]
+
+    expect(result).to eq result1
+  end
+
+  it 'タスク名とステータスで絞り込みできる' do
+    FactoryBot.create(:task)
+    FactoryBot.create(:second_task)
+    FactoryBot.create(:third_task)
+    result1 = FactoryBot.create(:forth_task)
+
+    result = Task.search(task = {name: 'test_task_01', status: '完了'})[0]
+
+    expect(result).to eq result1
+  end
+
 end
